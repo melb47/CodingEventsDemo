61a7daa5e6b199f3cdc001338dc49cb295da07e6
Melissa B <missmelissaboots@gmail.com>
2020-07-08 12:35:45 -0400

    all the way through to exercises

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -13,11 +13,15 @@ namespace coding_events_practice.Controllers
 {
     public class EventsController : Controller
     {
+        private EventDbContext context;
+        public EventsController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
-            List<Event> events = new List<Event>(EventData.GetAll());
-
+            List<Event> events = context.Events.ToList();
             return View(events);
         }
 
@@ -42,8 +46,8 @@ namespace coding_events_practice.Controllers
                     Type = addEventViewModel.Type
                 };
 
-                EventData.Add(newEvent);
-
+                context.Events.Add(newEvent);
+                context.SaveChanges();
                 return Redirect("/Events");
             }
 
@@ -52,8 +56,7 @@ namespace coding_events_practice.Controllers
 
         public IActionResult Delete()
         {
-            //ViewBag.title = "Delete Events";
-            ViewBag.events = EventData.GetAll();
+            ViewBag.events = context.Events.ToList();
 
             return View();
         }
@@ -63,9 +66,10 @@ namespace coding_events_practice.Controllers
         {
             foreach (int eventId in eventIds)
             {
-                EventData.Remove(eventId);
+                Event theEvent = context.Events.Find(eventId);
+                context.Events.Remove(theEvent);
             }
-
+            context.SaveChanges();
             return Redirect("/Events");
         }
     }
